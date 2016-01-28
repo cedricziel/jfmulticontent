5548ef0195f91e5109b32c1c787b1dfe43d458d4
Cedric Ziel <cedric@cedric-ziel.com>
2016-01-28 13:12:01 +0100

    [TASK] Remove loadTCA call
    TCA is always loaded.

--- ext_tables.php
+++ ext_tables.php
@@ -166,7 +166,6 @@ if ($confArr['useStoragePidOnly']) {
     );
 }
 
-\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('tt_content');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns, 1);
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'tx_jfmulticontent_view,tx_jfmulticontent_pages,tx_jfmulticontent_contents,tx_jfmulticontent_irre,pi_flexform';
