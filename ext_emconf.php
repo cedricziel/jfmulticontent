c3515a9fe6222afcff3863814770a9ca59f024f7
Cedric Ziel <cedric@cedric-ziel.com>
2016-01-28 14:50:20 +0100

    [TASK] Bump required core version to 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'cms' => '',
             'php' => '5.5.0-0.0.0',
-            'typo3' => '4.3.0-7.6.99',
+            'typo3' => '6.2.0-7.6.99',
         ),
         'conflicts' => array(
         ),
